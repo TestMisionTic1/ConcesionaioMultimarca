21549843b8dc35408b9f67c12e2834aafbdb3a87
David Puerto <dsp5502@gmail.com>
2021-10-06 16:59:13 -0500

    agregar roouter generico layouts2

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = () => {
     return (
         <div>
-            contenido Landing consesionario
+            contenido Landing consesionario1
         </div>
     )
 }
